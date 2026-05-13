2448780b72003cc44889e8d6db3dc919ae4461ef
svetlohub <waffle.leopold@gmail.com>
2026-05-13 09:30:28 +0300

    Update common.dart

--- lib/models/common.dart
+++ lib/models/common.dart
@@ -554,3 +554,32 @@ class Script with _$Script {
 
   factory Script.fromJson(Map<String, Object?> json) => _$ScriptFromJson(json);
 }
+
+@freezed
+class SubscriptionInfo with _$SubscriptionInfo {
+  const factory SubscriptionInfo({
+    @Default(0) int upload,
+    @Default(0) int download,
+    @Default(0) int total,
+    @Default(0) int expire,
+  }) = _SubscriptionInfo;
+
+  factory SubscriptionInfo.fromJson(Map<String, Object?> json) =>
+      _$SubscriptionInfoFromJson(json);
+
+  factory SubscriptionInfo.formHString(String? info) {
+    if (info == null) return const SubscriptionInfo();
+    final list = info.split(";");
+    final map = <String, int?>{};
+    for (final i in list) {
+      final keyValue = i.trim().split("=");
+      map[keyValue[0]] = int.tryParse(keyValue[1]);
+    }
+    return SubscriptionInfo(
+      upload: map["upload"] ?? 0,
+      download: map["download"] ?? 0,
+      total: map["total"] ?? 0,
+      expire: map["expire"] ?? 0,
+    );
+  }
+}
